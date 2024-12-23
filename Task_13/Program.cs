e5708d0661d20ec766a1dc6e89ccb5f04b0cce68
Dimitry-P <dimitry.piatnitsky@yahoo.com>
2024-12-23 16:32:23 +0300

    Add project files.

---
+++ Task_13/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_13
+{
+    internal class Program
+    {
+        //•	Задача 13: Напишите программу, которая выводит все простые числа до N.
+        public static string number;
+        public static int result;
+        public static int timer = 0;
+        public static int simple = 0;
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите любое число больше 1");
+            number = Console.ReadLine();
+            result = int.Parse(number);
+            Console.WriteLine("Простые числа: ");
+            Console.Write(2);
+            for (int i = 3; i < result+1; i++)
+            {
+                for(int j = 2; j < result+1; j++)
+                {
+                    if (j == i) { 
+                        Console.Write(", "+i );
+                    }
+                    if (i % j == 0) break;
+                }
+            }
+        }
+    }
+}
